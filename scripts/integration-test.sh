ba462c0fcab08751007474a5eddabb2292ef1d5a
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-07-08 12:36:32 +0300

    server start & cleanup

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -10,7 +10,7 @@ set -Eeuo pipefail
 # set log id and use shared log function for readable logs
 declare mydir
 mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-declare HOPR_LOG_ID="hopr-connect integration test"
+declare HOPR_LOG_ID="hopr-connect-test"
 source "${mydir}/../scripts/utils.sh"
 
 usage() {
@@ -19,11 +19,28 @@ usage() {
   msg
 }
 
+function cleanup {
+  local EXIT_CODE=$?
+
+  trap - SIGINT SIGTERM ERR EXIT
+
+  # Cleaning up everything
+  log "Stopping up processes"
+  for pid in ${server_pid}; do
+    log "${pid}"
+    kill -9 ${pid}
+  done
+
+  exit $EXIT_CODE
+}
+trap cleanup SIGINT SIGTERM ERR EXIT
+
 # return early with help info when requested
 ([ "${1:-}" = "-h" ] || [ "${1:-}" = "--help" ]) && { usage; exit 0; }
 
 declare exit_code=0
 
+# check prerequisites
 which yarn > /dev/null || exit_code=$?
 
 if [[ "${exit_code}" != 0 ]]; then
@@ -38,3 +55,19 @@ if [[ "${yarn_version_parsed[0]}" != "2" ]]; then
     exit 1
 fi
 
+# setup paths
+declare tmp="/tmp"
+declare server_pid
+declare server_log="${tmp}/hopr-connect-server.log"
+
+log "Running server..."
+
+DEBUG=hopr-connect*,simple-peer \
+yarn dlx \
+ts-node \
+    examples/server.ts \
+    > "${server_log}" \
+    2>&1 &
+
+server_pid=$!
+log "Server started with PID ${server_pid}"
