e2c2939f16d24c5661eeb791efeab829e643ed80
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-07-09 12:50:17 +0300

    fixed utils path

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -8,7 +8,7 @@ test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; 
 declare mydir
 mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 declare HOPR_LOG_ID="hopr-connect-test"
-source "${mydir}/../scripts/utils.sh"
+source "${mydir}/utils.sh"
 
 # exit on errors, undefined variables, ensure errors in pipes are not hidden
 set -Eeuo pipefail
