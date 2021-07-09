f7da6b7f90d58bb2f11a89f73a9c94b478125f8c
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-07-09 16:39:14 +0300

    cleaned

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 
-# prevent execution of this script, only allow execution
+# prevent execution of this script, only allow sourcing
 $(return >/dev/null 2>&1)
 test "$?" -eq "0" || { echo "This script should only be sourced." >&2; exit 1; }
 
 # exit on errors, undefined variables, ensure errors in pipes are not hidden
 set -Eeuo pipefail
-
-# $1=version string, semver
-function get_version_maj_min() {
-  echo $(get_version_maj_min_pat $1 | cut -d. -f1,2)
-}
-
-# $1=version string, semver
-function get_version_maj_min_pat() {
-  # From https://github.com/cloudflare/semver_bash/blob/master/semver.sh
-  # Fixed https://github.com/cloudflare/semver_bash/issues/4
-  local RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\).*'
-  local MAJ=$(echo "$1" | sed -e "s#$RE#\1#")
-  local MIN=$(echo "$1" | sed -e "s#$RE#\2#")
-  local PAT=$(echo "$1" | sed -e "s#$RE#\3#")
-  echo "$MAJ.$MIN.$PAT"
-}
 
 # $1 = port
 function ensure_port_is_free() {
@@ -32,20 +16,6 @@ function ensure_port_is_free() {
     log "Process: $(lsof -t -i ":${port}" -s TCP:LISTEN || :)"
     exit 1
   fi
-}
-
-# $1 = port to wait for
-# $2 = optional: file to tail for debug info
-# $3 = optional: delay between checks in seconds, defaults to 2s
-# $4 = optional: max number of checks, defaults to 1000
-function wait_for_http_port() {
-  local port=${1}
-  local log_file=${2:-}
-  local delay=${3:-2}
-  local max_wait=${4:-1000}
-  local cmd="curl --silent "localhost:${port}""
-
-  wait_for_port "${port}" "${log_file}" "${delay}" "${max_wait}" "${cmd}"
 }
 
 # $1 = file to monitor
@@ -65,34 +35,6 @@ function wait_for_regex_in_file() {
         return 0
       fi      
     done
-}
-
-# $1 = port to wait for
-# $2 = optional: file to tail for debug info
-# $3 = optional: delay between checks in seconds, defaults to 2s
-# $4 = optional: max number of checks, defaults to 1000
-# $5 = optional: command to check
-function wait_for_port() {
-  local port=${1}
-  local log_file=${2:-}
-  local delay=${3:-2}
-  local max_wait=${4:-1000}
-  # by default we do a basic listen check
-  local cmd=${5:-lsof -i ":${port}" -s TCP:LISTEN}
-
-  i=0
-  until ${cmd}; do
-    log "Waiting (${delay}) seconds for port ${port}"
-    if [ -s "${log_file}" ]; then
-      log "Last 5 logs:"
-      tail -n 5 "${log_file}" | sed "s/^/\\t/"
-    fi
-    sleep ${delay}
-    ((i=i+1))
-    if [ $i -gt ${max_wait} ]; then
-      exit 1
-    fi
-  done
 }
 
 setup_colors() {
