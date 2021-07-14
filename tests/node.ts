1146c72b2137dfb31df14c6310f2d8e50e80c7ba
HOPR Linting robot <noreply@hoprnet.org>
2021-07-14 10:02:04 +0000

    Fix linting

--- tests/node.ts
+++ tests/node.ts
@@ -16,13 +16,19 @@ import LibP2P from 'libp2p'
 
 const TEST_PROTOCOL = '/hopr-connect/test/0.0.1'
 
-async function startNode({ peerId, port, bootstrapAddress, noDirectConnections, noWebRTCUpgrade }: {
-  peerId: PeerId,
-  port: number,
-  bootstrapAddress?: Multiaddr,
-  noDirectConnections: boolean,
-  noWebRTCUpgrade: boolean,
-}) {  
+async function startNode({
+  peerId,
+  port,
+  bootstrapAddress,
+  noDirectConnections,
+  noWebRTCUpgrade
+}: {
+  peerId: PeerId
+  port: number
+  bootstrapAddress?: Multiaddr
+  noDirectConnections: boolean
+  noWebRTCUpgrade: boolean
+}) {
   console.log(`starting node, bootstrap address ${bootstrapAddress}`)
   const node = await libp2p.create({
     peerId,
@@ -41,7 +47,7 @@ async function startNode({ peerId, port, bootstrapAddress, noDirectConnections, 
           // simulates a NAT
           // DO NOT use this in production
           __noDirectConnections: noDirectConnections,
-          __noWebRTCUpgrade: noWebRTCUpgrade,
+          __noWebRTCUpgrade: noWebRTCUpgrade
         }
       },
       peerDiscovery: {
@@ -77,32 +83,33 @@ async function startNode({ peerId, port, bootstrapAddress, noDirectConnections, 
   return node
 }
 
-type CmdDef = {
-  cmd: 'wait'
-  waitForSecs: number
-} | {
-  cmd: 'dial',
-  targetIdentityName: string,
-  targetPort: number,
-} | {
-  cmd: 'msg',
-  msg: string,
-  targetIdentityName: string,
-  relayIdentityName: string,
-}
+type CmdDef =
+  | {
+      cmd: 'wait'
+      waitForSecs: number
+    }
+  | {
+      cmd: 'dial'
+      targetIdentityName: string
+      targetPort: number
+    }
+  | {
+      cmd: 'msg'
+      msg: string
+      targetIdentityName: string
+      relayIdentityName: string
+    }
 
-async function executeCommands({ node, cmds }: { node: LibP2P, cmds: CmdDef[] }) {
-  for(const cmdDef of cmds) {
-    switch(cmdDef.cmd) {
-      case 'wait':
-      {
+async function executeCommands({ node, cmds }: { node: LibP2P; cmds: CmdDef[] }) {
+  for (const cmdDef of cmds) {
+    switch (cmdDef.cmd) {
+      case 'wait': {
         console.log(`waiting ${cmdDef.waitForSecs} secs`)
         await new Promise((resolve) => setTimeout(resolve, durations.seconds(cmdDef.waitForSecs)))
         console.log(`finished waiting`)
         break
       }
-      case 'dial': 
-      {
+      case 'dial': {
         const targetPeerId = await peerIdForIdentity(cmdDef.targetIdentityName)
         const targetAddress = new Multiaddr(`/ip4/127.0.0.1/tcp/${cmdDef.targetPort}/p2p/${targetPeerId.toB58String()}`)
         console.log(`dialing ${cmdDef.targetIdentityName}`)
@@ -110,8 +117,7 @@ async function executeCommands({ node, cmds }: { node: LibP2P, cmds: CmdDef[] })
         console.log(`dialed`)
         break
       }
-      case 'msg':
-      {
+      case 'msg': {
         const targetPeerId = await peerIdForIdentity(cmdDef.targetIdentityName)
         const relayPeerId = await peerIdForIdentity(cmdDef.relayIdentityName)
         //@ts-ignore
@@ -123,22 +129,21 @@ async function executeCommands({ node, cmds }: { node: LibP2P, cmds: CmdDef[] })
           TEST_PROTOCOL
         )
         console.log(`piping msg: ${cmdDef.msg}`)
-        
+
         await pipe([new TextEncoder().encode(cmdDef.msg)], conn.stream, async (source: Stream['source']) => {
           for await (const msg of source) {
             const decoded = new TextDecoder().decode(msg.slice())
 
             console.log(`Received <${decoded}>`)
           }
-        })  
-        console.log(`sent msg`) 
-        break              
+        })
+        console.log(`sent msg`)
+        break
       }
-      default: 
-      {
+      default: {
         throw new Error(`unknown cmd: ${cmdDef}`)
       }
-    } 
+    }
   }
 }
 
@@ -156,44 +161,50 @@ async function main() {
     })
     .option('bootstrapPort', {
       describe: 'bootstrap node port',
-      type: 'number',
+      type: 'number'
     })
     .option('bootstrapIdentityName', {
       describe: 'identity name of a boostrap server',
-      choices: ['alice', 'bob', 'charly', 'dave', 'ed'],
+      choices: ['alice', 'bob', 'charly', 'dave', 'ed']
     })
     .option('noDirectConnections', {
       type: 'boolean',
-      demandOption: true,
+      demandOption: true
     })
     .option('noWebRTCUpgrade', {
       type: 'boolean',
-      demandOption: true,
+      demandOption: true
     })
     .option('command', {
       describe: 'example: --command.name dial --command.targetIdentityName charly',
-      type: 'string',
+      type: 'string'
     })
     .option('script', {
       type: 'string',
-      demandOption: true,
+      demandOption: true
     })
     .coerce({
-      script: input => JSON.parse(input.replace(/'/g, '"')) 
+      script: (input) => JSON.parse(input.replace(/'/g, '"'))
     })
     .parseSync()
 
   let bootstrapAddress: Multiaddr | undefined
-  
-  if(argv.bootstrapPort != null && argv.bootstrapIdentityName != null) {
-    const bootstrapPeerId = await peerIdForIdentity(argv.bootstrapIdentityName)  
+
+  if (argv.bootstrapPort != null && argv.bootstrapIdentityName != null) {
+    const bootstrapPeerId = await peerIdForIdentity(argv.bootstrapIdentityName)
     bootstrapAddress = new Multiaddr(`/ip4/127.0.0.1/tcp/${argv.bootstrapPort}/p2p/${bootstrapPeerId.toB58String()}`)
   }
   const peerId = await peerIdForIdentity(argv.identityName)
 
   console.log(`running node ${argv.identityName} on port ${argv.port}`)
-  const node = await startNode({ peerId, port: argv.port, bootstrapAddress, noDirectConnections: argv.noDirectConnections, noWebRTCUpgrade: argv.noWebRTCUpgrade })
-  
+  const node = await startNode({
+    peerId,
+    port: argv.port,
+    bootstrapAddress,
+    noDirectConnections: argv.noDirectConnections,
+    noWebRTCUpgrade: argv.noWebRTCUpgrade
+  })
+
   await executeCommands({ node, cmds: argv.script })
 }
 
--- tests/util.ts
+++ tests/util.ts
@@ -1,6 +1,6 @@
-import PeerId from "peer-id"
-import { getIdentity } from "./identities"
+import PeerId from 'peer-id'
+import { getIdentity } from './identities'
 
 export async function peerIdForIdentity(identityName: string): Promise<PeerId> {
-    return PeerId.createFromPrivKey(getIdentity(identityName))
+  return PeerId.createFromPrivKey(getIdentity(identityName))
 }
