eaad5bafeb8a92fe7634f943c7333786bc9e2351
HOPR Linting robot <noreply@hoprnet.org>
2021-07-29 10:19:58 +0000

    Fix linting

--- src/webrtc/connection.ts
+++ src/webrtc/connection.ts
@@ -308,7 +308,7 @@ class WebRTCConnection implements MultiaddrConnection {
     if (this._webRTCAvailable) {
       this.verbose(`FLOW: sending UPGRADED to relay`)
       this.relayConn.sendUpgraded()
-      
+
       // WebRTC handshake was successful, now using direct connection
       this._sinkMigrated = true
       if (this._sourceMigrated) {
